0ca659e16a9365f2300d4f6f88ef9eefe99b60f2
ActiveThumbs <ikman15b@gmail.com>
2024-03-04 17:48:14 +0100

    Add project files.

---
+++ PomodoroTimer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace PomodoroTimer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("==============Welcome to Pomodoro Timer!===============");
+
+            while (true)
+            {
+                StartTimer(1); // 25 minutes work interval
+                Console.WriteLine("Time's up! Take a short break.\n");
+                Beeps();
+                StartTimer(5); // 5 minutes break interval
+                Console.WriteLine("Break's over! Let's get back to work.\n");
+            }
+        }
+
+        static void StartTimer(int minutes)
+        {
+            int seconds = minutes * 60;
+            while (seconds > 0)
+            {
+                Console.Write($"\r{TimeSpan.FromSeconds(seconds).ToString(@"hh\:mm\:ss")}");
+                Thread.Sleep(1000);
+                seconds--;
+            }
+        }
+
+        static void Beeps()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Beep();
+            }
+        }
+    }
+}
+
